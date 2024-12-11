6bf76168a7a6ade7f666721e5181775b4178f5ad
Israel Estrada <programacion@shopeando.mx>
2024-12-11 17:14:28 -0600

    Understanding Middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SumService } from './sum/sum.service';
 import { SaynameModule } from './sayname/sayname.module';
+import { AuthcheckMiddleware } from './middleware/authcheck.middleware';
 
 @Module({
   imports: [SaynameModule],
   controllers: [AppController],
   providers: [AppService, SumService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthcheckMiddleware).forRoutes({path: '*', method: RequestMethod.ALL});
+  }
+}
---
+++ src/middleware/authcheck.middleware.ts
@@ -0,0 +1,11 @@
+import { NestMiddleware } from "@nestjs/common";
+import { NextFunction, Request, Response } from "express";
+
+export class AuthcheckMiddleware implements NestMiddleware {
+    use(req: Request, res: Response, next: NextFunction) {
+        if(req.headers.authorization) {
+            return next(); // Importante: usamos 'return' para terminar el flujo aquí
+        }
+        res.status(403).json({code: 403, message: "Not authorized"}); // 'return' asegura que no continúa el flujo
+    }
+}
